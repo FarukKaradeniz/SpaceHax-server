d752f9a98c50c83b325e1e28b3272610e54a3c41
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2021-03-03 11:49:56 +0300

    route role fix

--- app/routes/admin.routes.go
+++ app/routes/admin.routes.go
@@ -13,7 +13,7 @@ func AdminRoutes(app fiber.Router) {
 
 	config := r.Group("/configs")
 	config.Post("", middleware.Protected([]string{"admin"}), services.AddRoomConfig)
-	config.Put("/:alias", middleware.Protected([]string{"admin"}), services.UpdateConfig)
+	config.Put("/:alias", middleware.Protected([]string{"admin", "room"}), services.UpdateConfig)
 	config.Delete("/:alias", middleware.Protected([]string{"admin"}), services.RemoveConfig)
 	config.Get("/:alias?", middleware.Protected([]string{"room", "admin"}), services.GetRoomConfig)
 }
